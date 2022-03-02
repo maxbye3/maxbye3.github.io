6614fa14d6a3c9d08d0cbdbb96241a4202c00b17
Max Bye <mbye@techsoup.org>
2022-03-02 17:18:22 +0000

    video overlays working

--- chekhov_hwacha/js/index.js
+++ chekhov_hwacha/js/index.js
@@ -1,10 +1,30 @@
 const mainVideo = document.getElementById('mainVideo')
 const mapVideo = document.getElementById('mapVideo')
-function startVideo() {
-  console.log('hit!')
-  document.getElementById('disclaimer').style.display = 'none'
-  mainVideo.play()
-}
+let initialClick = true
+
 setInterval(() => {
   mapVideo.currentTime = mainVideo.currentTime
 }, 100)
+
+const touchdown = () => {
+  if (initialClick) {
+    document.getElementById('disclaimer').style.display = 'none'
+    mainVideo.play()
+    mapVideo.play()
+    initialClick = false
+    return
+  }
+  mainVideo.style.opacity = 0
+  // console.log('touch down')
+}
+const touchup = () => {
+  mainVideo.style.opacity = 100
+  // console.log('touch up')
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  window.addEventListener('mousedown', touchdown, false)
+  window.addEventListener('mouseup', touchup, false)
+  window.addEventListener('touchstart', touchdown, false)
+  window.addEventListener('touchend', touchup, false)
+})
